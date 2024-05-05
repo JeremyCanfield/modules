975a5e623d5958c6fb18bd4de08a146f8495f5f2
Jeremy Canfield <jeremy.canfield@freekb.net>
2024-05-05 06:22:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 from my_pip_package import __version__
 
 setup(
-    name='my_pip_package',
+    name='greeting',
     version=__version__,
 
     url='https://github.com/JeremyCanfield/modules',
     author='Jeremy Canfield',
     author_email='jeremy.canfield@freekb.net',
 
-    py_modules=['my_pip_package'],
+    py_modules=['greeting'],
 )
