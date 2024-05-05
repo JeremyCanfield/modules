611758ff4da5b80a5cea0edf923ce0dfab98ca3a
Jeremy Canfield <jeremy.canfield@freekb.net>
2024-05-05 06:34:19 -0500

    Update and rename greeting.py to foo/greeting.py

---
+++ foo/greeting.py
@@ -0,0 +1,4 @@
+__version__ = '0.0.3'
+
+def hello():
+    return "Hello World 0.0.3"
--- greeting.py
+++
@@ -1,7 +0,0 @@
-__version__ = '0.0.2'
-
-def greeting():
-    print("Hello World")
-
-def hello():
-    return "Hello World"
